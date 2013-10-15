a4ff45111e86e4cddb04c92bb23f70b37ae54853
Philip Semanchuk <philip@PySpoken.com>
2013-10-15 00:34:30 +0000

    adding new demo for new context manager feature.

---
+++ demo4/child.py
@@ -0,0 +1,21 @@
+import posix_ipc
+import time
+import sys
+import random
+
+# The parent passes the semaphore's name to me.
+name = sys.argv[1]
+
+print('Child: waiting to aquire semaphore ' + name)
+
+with posix_ipc.Semaphore(name) as sem: 
+    print('Child: semaphore ' + sem.name + ' aquired; holding for 3 seconds.')
+
+    # Flip a coin to determine whether or not to bail out of the context.
+    if random.randint(0, 1):
+        print("Child: raising ValueError to demonstrate unplanned context exit")
+        raise ValueError
+
+    time.sleep(3)
+
+    print('Child: gracefully exiting context (releasing the semaphore).')
---
+++ demo4/parent.py
@@ -0,0 +1,46 @@
+import subprocess
+import posix_ipc
+import time
+import os
+
+sem = posix_ipc.Semaphore(None, posix_ipc.O_CREX, initial_value = 1)
+print("Parent: created semaphore {}.".format(sem.name))
+
+sem.acquire()
+
+# Spawn a child that will wait on this semaphore.
+path, _ = os.path.split(__file__)
+print("Parent: spawning child process...")
+subprocess.Popen(["python", os.path.join(path, 'child.py'), sem.name])
+
+for i in range(3, 0, -1):
+    print("Parent: child process will acquire the semaphore in {} seconds...".format(i))
+    time.sleep(1)
+
+sem.release()
+
+# Sleep for a second to give the child a chance to acquire the semaphore. 
+# This technique is a little sloppy because technically the child could still 
+# starve, but it's certainly sufficient for this demo.
+time.sleep(1)
+
+# Wait for the child to release the semaphore.
+print("Parent: waiting for the child to release the semaphore.")
+sem.acquire()
+
+# Clean up.
+print("Parent: destroying the semaphore.")
+sem.release()
+sem.unlink()
+
+msg = """ 
+By the time you're done reading this, the parent will have exited and so the
+operating system will have destroyed the semaphore. You can prove that  the
+semaphore is gone by running this command and observing that it raises
+posix_ipc.ExistentialError --
+
+   python -c "import posix_ipc; posix_ipc.Semaphore('{}')"
+
+""".format(sem.name)
+
+print(msg)
