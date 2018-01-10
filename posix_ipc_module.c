99c11986a896209560b5501bb987240ad506d34a
osvenskan <philip@pyspoken.com>
2018-01-10 08:25:03 -0500

    update copyright year in several places

--- posix_ipc_module.c
+++ posix_ipc_module.c
@@ -2546,7 +2546,7 @@ POSIX_IPC_INIT_FUNCTION_NAME(void) {
 
     PyModule_AddStringConstant(module, "VERSION", POSIX_IPC_VERSION);
     PyModule_AddStringConstant(module, "__version__", POSIX_IPC_VERSION);
-    PyModule_AddStringConstant(module, "__copyright__", "Copyright 2012 Philip Semanchuk");
+    PyModule_AddStringConstant(module, "__copyright__", "Copyright 2018 Philip Semanchuk");
     PyModule_AddStringConstant(module, "__author__", "Philip Semanchuk");
     PyModule_AddStringConstant(module, "__license__", "BSD");
 
